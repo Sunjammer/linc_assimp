3147b6d7c829a58e5225f15bd98a1f9d8f190edb
Andreas Rønning <andreas@doomsday.no>
2018-10-23 17:55:32 +0200

    Cleanup

--- linc/linc_assimp.cpp
+++ linc/linc_assimp.cpp
@@ -6,17 +6,6 @@ namespace linc {
 
     namespace assimp {
 
-        extern const aiScene* ReadFile(String path){
-            Assimp::Importer importer;
-            const aiScene* scene = importer.ReadFile(path.c_str(), aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenNormals);
-            if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) 
-            {
-                printf("ERROR::ASSIMP:: %s \n", importer.GetErrorString());
-                return 0;
-            }
-            return scene;
-        }
-
     } //assimp namespace
 
 } //linc
